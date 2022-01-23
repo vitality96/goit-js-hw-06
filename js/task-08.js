ceaa120f2f4e4c81c9b519bef20136549400d8ca
Vitalii Peresoliak <vitalik21022@gmail.com>
2022-01-23 19:02:49 +0200

    Виправив помилку

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,13 @@
 const formElement = document.querySelector('.login-form');
 
 
-
 const onFormSubmit = event => {
     event.preventDefault();
 
-    const formElement = event.currentTarget.elements;
+    const elementForm = event.currentTarget.elements;
 
-    const email = formElement.email.value;
-    const password = formElement.password.value;
+    const email = elementForm.email.value;
+    const password = elementForm.password.value;
 
         if (email === "" || password === "") {
             alert('All fields must be filled!!!');
